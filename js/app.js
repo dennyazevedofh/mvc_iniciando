40e8286b941ff4ca6ecc4dcb4a26bc57b08f03ff
Denny Azevedo <denny.filho@animaeducacao.com.br>
2025-02-26 09:40:18 -0300

    :construction: Incluindo Alunos :student:

--- js/app.js
+++ js/app.js
@@ -19,9 +19,9 @@ const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => {
 	return `<td>${materia}</td>`
 }).join('')
 
-console.log(htmlHeaderMaterias)
+//console.log(htmlHeaderMaterias)
 htmlHeader.innerHTML += htmlHeaderMaterias
-console.log(htmlHeader)
+//console.log(htmlHeader)
 document.querySelector('[data-table-alunos] thead').appendChild(htmlHeader)
 
 // Inserir no tbody da tabela a lista de alunos e suas médias
@@ -35,4 +35,34 @@ alunos.forEach(aluno => {
 
 	htmlRow.innerHTML += htmlRowMaterias
 	document.querySelector('[data-table-alunos] tbody').appendChild(htmlRow)
+})
+
+// Adicionar aluno
+document.querySelector('form').addEventListener('submit', function(e) {
+	e.preventDefault()
+	const nome = document.getElementById('first_name').value
+	// como forme de exemplo, para poder incluir um novo aluno ele deve obbrigatoriamente obdecer a estrutura de notas
+	// caso contrário, o código irá quebrar
+	// portanto, vamos neste momento usar a estrutura de notas existente de algum aluno e ajustar o nome
+	// para o novo aluno
+	// futuramente precisamos ajustar tudo, visto que o _id deve ser único.
+	const newAluno = {
+		_id: 0,
+		nome,
+		notas: {
+			backend_1: [1, 1, 2, 2],
+			frontend_2: [2, 2, 2, 2],
+			bancodados: [2, 2, 3, 3],
+			ferramentas: [3, 3, 3, 3],
+		},
+	}
+	//console.log(nome)
+	// é preciso calcular aa média das notas do novo aluno
+	// portanto vamos repetir uma parte do nosso código.
+	newAluno.media = {}
+	for(let materia in newAluno.notas) {
+		newAluno.media[materia] = average(...newAluno.notas[materia])
+	}
+
+	alunos.push(newAluno)
 })
